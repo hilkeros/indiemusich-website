ab0a48b26f96725a45301784958311df34a853eb
Hilke Ros <>
2025-03-16 15:03:51 +0100

    Fix env loading

--- indiemusich/settings.py
+++ indiemusich/settings.py
@@ -20,9 +20,8 @@ import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+# Load environment variables from .env file
+dotenv.read_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
