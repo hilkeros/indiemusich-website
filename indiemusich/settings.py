499b2bc301684c1d29c75f2dd369d319535766ba
Hilke Ros <>
2025-03-24 18:59:51 +0100

    Use correct is_prod

--- indiemusich/settings.py
+++ indiemusich/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = "indiemusich.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-if IS_PROD:
+if is_prod:
     # In production on Heroku the database configuration is derived from the `DATABASE_URL`
     # environment variable by the dj-database-url package. `DATABASE_URL` will be set
     # automatically by Heroku when a database addon is attached to your Heroku app. See:
